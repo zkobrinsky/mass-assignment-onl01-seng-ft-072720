e0adf66db230d49097c7e6bd7776cffeab9b46c9
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 19:33:33 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,11 @@
 class Person
-  #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+  :weight, :handed, :complexion, :t_shirt_size,
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+  def initialize(hash)
+    self_attributes = hash
+  end
+
+
 end
