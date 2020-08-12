ad0d4a4b442b9043819334aa93120c360a78ca87
Zach Kobrinsky <zachkobrinsky@gmail.com>
2020-08-12 19:37:19 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -6,8 +6,7 @@ class Person
   :wrist_size, :glove_size, :pant_length, :pant_width
 
   def initialize(hash)
-    hash.each{|key, value| self.send(("#{key}="), value)
-  binding.pry}
+    hash.each{|key, value| self.send(("#{key}="), value)}
 
   end
 
